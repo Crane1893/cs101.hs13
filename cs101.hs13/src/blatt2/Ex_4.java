9b9b8b3cdd0c44fed9519898623ebfe035cb9b59
Eric Jan <eric.jan@gmx.ch>
2013-10-07 16:36:08 +0200

    add Ex_4

---
+++ cs101.hs13/src/blatt2/Ex_4.java
@@ -0,0 +1,33 @@
+package blatt2;
+
+import ch.unibas.informatik.cs101.ImageWindow;
+
+public class Ex_4 {
+
+  public static void main(String args[]) {
+    //creates a new instance of the ImageWindow Class
+    //with a viewable image area of 500x500 pixels
+    ImageWindow w = new ImageWindow(500,500);
+
+    //opens the corresponding window (makes it visible)
+    w.openWindow();
+
+    //colors the pixel at position 255,100 in the image
+    //with the rgb color 254 (red), 0 (green), 0 (blue)
+    
+    int y = 456;
+    int vx = 20;
+    
+    for(int t=1;t<=500;t+=20){
+    	w.setPixel(t, y, 255, 0, 0);
+    	System.out.println("t: "+t+" h: "+y+" vy: ");
+    	
+    }
+
+    // Add your code here
+
+    //redraws the image on the screen so all changes
+    //become visible
+    w.redraw();
+  }
+}
